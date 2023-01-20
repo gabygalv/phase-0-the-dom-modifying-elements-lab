efdbbcdb617745f799b762077cdfa425098ef3ca
Gabriela Galvan <gabriela1galvan@gmail.com>
2023-01-20 10:21:53 -0700

    dom modyfying elements lab completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const newHeader = document.createElement("H1");
+newHeader.id= 'victory'
+newHeader.innerHTML='gaby is the champion'
+newHeader.className = 'victory'
+  document.querySelector("main#main").remove();
